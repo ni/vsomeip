326e8c7b2c6d4039717b637e613e64d76a27ef1f
ggrigory <garikgrigoryan96@gmail.com>
2026-09-24 14:31:31 +0400

    Silence unused-parameter warnings in XNET accept policy test

--- test/unit_tests/xnet_tests/ut_xnet_tcp_server_accept_policy.cpp
+++ test/unit_tests/xnet_tests/ut_xnet_tcp_server_accept_policy.cpp
@@ -55,15 +55,15 @@ public:
     void async_write(boost::asio::const_buffer const&, completion_condition, rw_handler _handler) override { _handler({}, 0); }
 
 #if defined(__linux__)
-    [[nodiscard]] bool set_user_timeout(unsigned int _timeout) override { return true; }
-    [[nodiscard]] bool set_keepidle(uint32_t _idle) override { return true; }
-    [[nodiscard]] bool set_keepintvl(uint32_t _interval) override { return true; }
-    [[nodiscard]] bool set_keepcnt(uint32_t _count) override { return true; }
+    [[nodiscard]] bool set_user_timeout([[maybe_unused]] unsigned int _timeout) override { return true; }
+    [[nodiscard]] bool set_keepidle([[maybe_unused]] uint32_t _idle) override { return true; }
+    [[nodiscard]] bool set_keepintvl([[maybe_unused]] uint32_t _interval) override { return true; }
+    [[nodiscard]] bool set_keepcnt([[maybe_unused]] uint32_t _count) override { return true; }
     [[nodiscard]] bool set_quick_ack() override { return true; }
 #endif
 
 #if defined(__linux__) || defined(__QNX__)
-    [[nodiscard]] bool bind_to_device(std::string const& _device) override { return true; }
+    [[nodiscard]] bool bind_to_device([[maybe_unused]] std::string const& _device) override { return true; }
     [[nodiscard]] bool can_read_fd_flags() override { return true; }
 #endif
 };
